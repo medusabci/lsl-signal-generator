65e1f85c66a7aa8d64cb187abf59150db82dcf3b
edsanva <edusan1213@gmail.com>
2021-06-15 10:47:44 +0200

    Initial commit signal-generator

---
+++ src/main.py
@@ -0,0 +1,129 @@
+"""
+Author:   Eduardo Santamaría-Vázquez
+Date:     09 June 2021
+Version:  0.1
+"""
+
+# PYTHON MODULES
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+from signal_generator import SignalGenerator
+from utils.gui_notifications import NotificationStack
+from utils import gui_utils
+import sys, os
+
+# Load the .ui file
+gui_main_user_interface = uic.loadUiType("signal_generator.ui")[0]
+
+# Status constants
+PD_NOT_CONNECTED = 'Not connected '
+PD_READY = 'Ready '
+PD_RECORDING = 'Recording... '
+LSL_NOT_SENDING = 'Not sending '
+LSL_SENDING = 'Transmitting... '
+
+
+class SignalGeneratorGUI(QMainWindow, gui_main_user_interface):
+
+    def __init__(self):
+        try:
+            QMainWindow.__init__(self)
+            # Setup UI
+            self.setupUi(self)
+            self.resize(400, 450)
+            # Initialize the notification stack
+            self.notifications = NotificationStack(parent=self)
+            # Initialize the application
+            self.dir = os.path.dirname(__file__)
+            self.stl = gui_utils.set_css_and_theme(
+                self, os.path.join('utils/gui_stylesheet.css'), 'dark')
+            # self.setWindowIcon(QIcon('icons/icon_photodiode.png'))
+            self.setWindowTitle('Signal generator')
+            # Current application status
+            self.current_status = None
+            self.set_status(PD_READY)
+            # Initialize the notification stack
+            self.notifications = NotificationStack(parent=self)
+            # Buttons
+            self.button_play.clicked.connect(self.on_play)
+            self.button_stop.clicked.connect(self.on_stop)
+            # Init signal generator
+            self.signal_generator = None
+            # Show the application
+            self.show()
+        except Exception as e:
+            self.notifications.new_notification('[ERROR] %s' % str(e))
+
+    def on_play(self):
+        try:
+            if self.current_status == PD_READY:
+                # Get info from GUI
+                stream_name = self.lineEdit_lsl_stream_name.text()
+                stream_type = self.lineEdit_lsl_stream_type.text()
+                chunk_size = self.spinBox_lsl_chunk_size.value()
+                format = self.lineEdit_lsl_format.text()
+                n_cha = self.spinBox_n_cha.value()
+                l_cha = self.lineEdit_l_cha.text().split(';')
+                units = self.lineEdit_signal_units.text()
+                mean = self.doubleSpinBox_signal_mean.value()
+                std = self.doubleSpinBox_signal_std.value()
+                sample_rate = self.doubleSpinBox_signal_sample_rate.value()
+                # Signal generator
+                self.signal_generator = SignalGenerator(stream_name=stream_name,
+                                                        stream_type=stream_type,
+                                                        chunk_size=chunk_size,
+                                                        format=format,
+                                                        n_cha=n_cha,
+                                                        l_cha=l_cha,
+                                                        units=units,
+                                                        mean=mean,
+                                                        std=std,
+                                                        sample_rate=sample_rate)
+                # Init the LSL stream
+                self.signal_generator.init_send_lsl()
+                # Modify the status
+                self.set_status(PD_RECORDING)
+        except Exception as e:
+            self.notifications.new_notification('[ERROR] %s' % str(e))
+
+    def on_stop(self):
+        try:
+            if self.current_status == PD_RECORDING:
+                # Close the LSL stream
+                self.signal_generator.close_lsl()
+                # Modify the status
+                self.set_status(PD_READY)
+        except Exception as e:
+            self.notifications.new_notification('[ERROR] %s' % str(e))
+
+    def set_status(self, status):
+        try:
+            # Status label and state
+            self.current_status = status
+            # Button enabling/disabling
+            if status == PD_READY:
+                self.button_play.setEnabled(True)
+                self.button_stop.setEnabled(False)
+            elif status == PD_RECORDING:
+                self.button_play.setEnabled(False)
+                self.button_stop.setEnabled(True)
+            else:
+                self.notifications.new_notification('Unknown status: ' + status)
+                print('Exception: ' + 'Unknown status: ' + status)
+        except Exception as e:
+            self.notifications.new_notification('[ERROR] %s' % str(e))
+
+    def closeEvent(self, event):
+        try:
+            # Let the window close
+            self.signal_generator.close()
+            event.accept()
+        except Exception as e:
+            self.notifications.new_notification('[ERROR] %s' % str(e))
+
+
+if __name__ == '__main__':
+    application = QApplication(sys.argv)
+    main_window = SignalGeneratorGUI()
+    sys.exit(application.exec_())
---
+++ src/signal_generator.py
@@ -0,0 +1,102 @@
+"""
+Author:   Eduardo Santamaría-Vázquez
+Date:     09 June 2021
+Version:  0.1
+"""
+
+from pylsl import StreamInfo, StreamOutlet, local_clock
+import time, threading
+import numpy as np
+
+
+class SignalGenerator:
+
+    def __init__(self, stream_name, stream_type, chunk_size, format, n_cha,
+                 l_cha, units, mean, std, sample_rate):
+
+        # Error check
+        if len(l_cha) != n_cha:
+            raise ValueError('The number of channel labels does not match with '
+                             'the number of channels')
+
+        # Generator UID
+        self.uid = "ce5de488-ca9f-11eb-b8bc-0242ac130003"
+
+        # Parameters
+        self.stream_name = stream_name
+        self.stream_type = stream_type
+        self.chunk_size = chunk_size
+        self.format = format
+        self.n_cha = n_cha
+        self.l_cha = l_cha
+        self.units = units
+        self.mean = mean
+        self.std = std
+        self.sample_rate = sample_rate
+
+        # Event for stopping the IO thread
+        self.io_run = threading.Event()
+        self.io_run.set()
+
+        # LSL
+        self.lsl_outlet = None
+        self.lsl_buffer = []
+        self.lsl_thread = None
+
+        # Run IO function in other thread
+        self.io_init_timestamp = None
+        self.io_thread = threading.Thread(
+            target=self.send_data,
+            args=[self.io_run]
+        )
+        self.io_thread.start()
+
+    def close(self):
+        self.io_run.clear()
+        self.io_thread.join()
+
+    def init_send_lsl(self):
+        # Create the steam outlet
+        lsl_info = StreamInfo(name=self.stream_name,
+                              type=self.stream_type,
+                              channel_count=self.n_cha,
+                              nominal_srate=self.sample_rate,
+                              channel_format=self.format,
+                              source_id=self.uid)
+        # lsl_info.desc().append_child_value("manufacturer", "")
+        channels = lsl_info.desc().append_child("channels")
+        for l in self.l_cha:
+            channels.append_child("channel") \
+                .append_child_value("label", l) \
+                .append_child_value("units", self.units) \
+                .append_child_value("type", self.stream_type)
+
+        self.lsl_outlet = StreamOutlet(info=lsl_info,
+                                       chunk_size=self.chunk_size,
+                                       max_buffered=360)
+        print('[SignalGenerator] > LSL stream created.')
+
+    def close_lsl(self):
+        self.lsl_outlet = None
+        self.lsl_buffer = []
+        print('[SignalGenerator] > LSL stream closed.')
+
+    # In Thread
+    def send_data(self, running_event):
+        while running_event.is_set():
+            # Assert that the parse to float is correct
+            try:
+                if self.lsl_outlet is not None:
+                    sample = self.std * np.random.randn(
+                        self.chunk_size, self.n_cha) + self.mean
+                    sample = sample.tolist()
+                    # Get a time stamp in seconds
+                    stamp = local_clock()
+                    # Now send it
+                    self.lsl_outlet.push_chunk(sample, stamp)
+                    # Wait
+                    time.sleep(self.chunk_size / self.sample_rate)
+            except Exception as e:
+                raise e
+        print('[SignalGenerator] > IO thread done.')
+
